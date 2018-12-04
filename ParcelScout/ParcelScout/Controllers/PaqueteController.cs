59e5a184ccbd4f0717a3ca8a9cabc6b05617d033
Gabriel Becerra <grbg200025@gmail.com>
2018-12-03 22:39:53 -0700

    Es agrego el metodo para agregar correos

--- ParcelScout/ParcelScout/Controllers/PaqueteController.cs
+++ ParcelScout/ParcelScout/Controllers/PaqueteController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
 
 namespace ParcelScout.Controllers
 {
@@ -190,6 +191,72 @@ namespace ParcelScout.Controllers
         //    return action;
         //}
 
+        public ActionResult CorreoNuevoPaquete(string correo, string nombrecliente, string nombredestinatario, string norastreo, string precio)
+        {
+            try
+            {
+                SmtpClient client = new SmtpClient();
+                client.Port = 587;
+                client.Host = "smtp.gmail.com";
+                client.EnableSsl = true;
+                client.Timeout = 1000000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new System.Net.NetworkCredential("parcelscout.isw@gmail.com", "scoutparcel");
+                MailMessage mmsg = new MailMessage();
+
+                mmsg.To.Add(correo);
+                mmsg.Subject = "Su paquete ha sido registrado";
+                mmsg.SubjectEncoding = System.Text.Encoding.UTF8;
+
+                mmsg.Body = bodyCliente.Replace("[NOMBRE]", nombrecliente);
+                mmsg.Body = bodyCliente.Replace("[DESTINATARIO]", nombredestinatario);
+                mmsg.Body = bodyCliente.Replace("[NO_RASTREO]", nombrecliente);
+                mmsg.Body = bodyCliente.Replace("[NOMBRE]", nombrecliente);
+
+                mmsg.BodyEncoding = System.Text.Encoding.UTF8;
+                mmsg.IsBodyHtml = true;
+                mmsg.From = new MailAddress("parcelscout.isw@gmail.com");
+                client.Send(mmsg);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return View();
+        }
+
+        static string bodyCliente = "<html><head><meta charset='UTF-8'><link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'><link href='https://fonts.googleapis.com/css?family=Merriweather|Merriweather+Sans|Open+Sans|Pacifico' rel='stylesheet'><style type='text/css'>" +
+            ".contenido{color: white;" +
+                "background-color: #2E8B57;" +
+                "border-radius: 20px;" +
+                "width: 500px;" +
+                "display: block;" +
+                "margin-left: auto;" +
+                "margin-right: auto;" +
+                "border-style: solid;" +
+                "border-bottom-width: 10px;" +
+                "border-color: #C66435;" +
+                "box-shadow: 3px 3px 10px #888888;}" +
+            "img" +
+        "{" +
+            "padding-top: 10px;" +
+            "padding-right: 50px;" +
+            "display: block;" +
+            "margin-left: auto;" +
+            "margin-right: auto;" +
+            "width: 200px;" +
+            "height: auto;" +
+        "}" +
+        "h2{text-align: center;" +
+                "font-family: Pacifico;}" +
+    "h3{text-align: center;" +
+                "font-family: Roboto;" +
+                "padding-bottom: 10px;}" +
+"h4, h5{text-align: center;" +
+                "font-family: Roboto;}" +
+            ".ultimo{padding-bottom: 20px;}" +
+        "</style></head><body><div class='contenido'><img src = 'Logo.png' >< h2 >¡Hola, [NOMBRE]!</h2><h3>¡Tu envío para[DESTINATARIO] ya ha sido registrado!</h3><h4>Numero de guía:</h4><h5>[NO_RASTREO]</h5><H4>Costo:</H4><h5 class='ultimo'>[PRECIO]</h5></div></body></html>";
 
     }
 }
